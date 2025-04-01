5ae3992831427dfbd7d22eaa8a95f9da08bdb67a
Nursultan Sh <shohanovn@gmail.com>
2025-04-01 11:55:17 +0500

    Main

--- app/src/main/java/com/example/countriesinfolab6/MainActivity.kt
+++ app/src/main/java/com/example/countriesinfolab6/MainActivity.kt
@@ -1,47 +1,35 @@
 package com.example.countriesinfolab6
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.countriesinfolab6.ui.theme.CountriesInfoLab6Theme
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CountryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CountriesInfoLab6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val countries = listOf(
+            Country("Vietnam", R.mipmap.vn, 98000000),
+            Country("United States", R.mipmap.us, 320000000),
+            Country("Russia", R.mipmap.ru, 142000000)
+        )
+
+        adapter = CountryAdapter(countries) { country ->
+            Toast.makeText(
+                this,
+                "Selected: ${country.name}", // Используем name вместо countryName
+                Toast.LENGTH_SHORT
+            ).show()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CountriesInfoLab6Theme {
-        Greeting("Android")
+        recyclerView.adapter = adapter
     }
 }
